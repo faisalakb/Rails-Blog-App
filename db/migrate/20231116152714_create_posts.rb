a07cfb415a3c6af0bf8e26d418d9535e46efa96c
faisalakb <faisalakb786@gmail.com>
2023-11-16 20:43:04 +0500

    add models

--- db/migrate/20231116152714_create_posts.rb
+++ db/migrate/20231116152714_create_posts.rb
@@ -3,9 +3,9 @@ class CreatePosts < ActiveRecord::Migration[7.1]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.integer :comments_counter, default: 0
-      t.integer :likes_counter, default: 0
-      t.integer :author_id 
+      t.integer :comments_counter
+      t.integer :likes_counter
+      t.integer :author_id
 
       t.timestamps
     end

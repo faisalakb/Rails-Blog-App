79999c6f93c74fe1a83ab1e7af144185f6997983
faisalakb <faisalakb786@gmail.com>
2023-11-17 18:42:26 +0500

    Add methods in model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  belongs_to :user, class_name: 'User', foreign_key: :user_id
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_15_151208) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_15_145825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,9 +53,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_151208) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", on_delete: :cascade
-  add_foreign_key "comments", "users", on_delete: :cascade
-  add_foreign_key "likes", "posts", on_delete: :cascade
-  add_foreign_key "likes", "users", on_delete: :cascade
-  add_foreign_key "posts", "users", column: "author_id", on_delete: :cascade
 end

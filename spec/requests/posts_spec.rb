1c3805328f2d270ac41833503c35ba205151f4a2
faisalakb <faisalakb786@gmail.com>
2023-11-22 20:48:34 +0500

    Update linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /users/:user_id/posts' do
     it 'returns http success' do
-      get "/users/:user_id/posts"
+      get '/users/:user_id/posts'
       expect(response).to have_http_status(:success)
     end
   end

b8135abc6fa6d27f05811ca2dfad758156e852f2
faisalakb <faisalakb786@gmail.com>
2023-11-28 12:37:09 +0500

    Add test for view users

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
+
+Capybara.configure do |config|
+  config.default_driver = :selenium # or :poltergeist, :webkit, etc.
+  # Other Capybara configurations go here
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
